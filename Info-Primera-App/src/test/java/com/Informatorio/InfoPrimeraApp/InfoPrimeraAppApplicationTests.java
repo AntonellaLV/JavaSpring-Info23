88dd1371d8ec3256c70a1593ec77e9562a618ca9
AntonellaLV <118840064+AntonellaLV@users.noreply.github.com>
2023-12-03 22:38:15 -0300

    Add Files

---
+++ Info-Primera-App/src/test/java/com/Informatorio/InfoPrimeraApp/InfoPrimeraAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Informatorio.InfoPrimeraApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoPrimeraAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
