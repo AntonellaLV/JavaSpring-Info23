4c58f4c5b260f55af1cc8969d20589abd5625921
AntonellaLV <118840064+AntonellaLV@users.noreply.github.com>
2023-12-04 19:10:53 -0300

    Add

--- Info-Primera-App/src/main/java/com/Informatorio/InfoPrimeraApp/BreakingmarketApplication.java
+++ Info-Primera-App/src/main/java/com/Informatorio/InfoPrimeraApp/BreakingmarketApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InfoPrimeraAppApplication {
+public class BreakingmarketApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InfoPrimeraAppApplication.class, args);
+		SpringApplication.run(BreakingmarketApplication.class, args);
 	}
 
 }
---
+++ Info-Primera-App/src/main/java/com/Informatorio/InfoPrimeraApp/dominio/Proveedor.java
@@ -0,0 +1,4 @@
+package com.Informatorio.InfoPrimeraApp.dominio;
+
+public class Proveedor {
+}
